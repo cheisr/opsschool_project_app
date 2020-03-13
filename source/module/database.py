8b9c4bc7bcfa752786863860c7865eca41468128
cheisr <tavorcheni@gmail.com>
2020-03-14 00:09:45 +0200

    added source files

---
+++ source/module/database.py
@@ -0,0 +1,77 @@
+
+
+import pymysql
+import os
+
+mysqlhostip = os.getenv('MYSQL_ADDRESS','localhost')
+mysqluser = os.getenv('MYSQL_USER','shani')
+mysqlpassword = os.getenv('MYSQL_PWD')
+mysqldb = os.getenv('MYSQL_DB','crud_flask')
+
+class Database:
+    def connect(self):
+        return pymysql.connect(host=mysqlhostip,user=mysqluser,password=mysqlpassword,db=mysqldb)
+
+    def read(self, id):
+        con = Database.connect(self)
+        cursor = con.cursor()
+
+        try:
+            if id == None:
+                cursor.execute("SELECT * FROM phone_book order by name asc")
+            else:
+                cursor.execute("SELECT * FROM phone_book where id = %s order by name asc", (id,))
+
+            return cursor.fetchall()
+        except:
+            return ()
+        finally:
+            con.close()
+
+    def insert(self,data):
+        con = Database.connect(self)
+        cursor = con.cursor()
+
+        try:
+            cursor.execute("INSERT INTO phone_book(name,phone,address) VALUES(%s, %s, %s)", (data['name'],data['phone'],data['address'],))
+            con.commit()
+
+            return True
+        except:
+            con.rollback()
+
+            return False
+        finally:
+            con.close()
+
+    def update(self, id, data):
+        con = Database.connect(self)
+        cursor = con.cursor()
+
+        try:
+            cursor.execute("UPDATE phone_book set name = %s, phone = %s, address = %s where id = %s", (data['name'],data['phone'],data['address'],id,))
+            con.commit()
+
+            return True
+        except:
+            con.rollback()
+
+            return False
+        finally:
+            con.close()
+
+    def delete(self, id):
+        con = Database.connect(self)
+        cursor = con.cursor()
+
+        try:
+            cursor.execute("DELETE FROM phone_book where id = %s", (id,))
+            con.commit()
+
+            return True
+        except:
+            con.rollback()
+
+            return False
+        finally:
+            con.close()
